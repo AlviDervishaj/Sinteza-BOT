ee2ff3ac5929adab61209e8d7a395cdcb6e4bffc
Alvi Dervishaj <alvidervishaj9@gmail.com>
2023-07-14 13:10:28 +0200

    Config Files

--- pages/api/getBusNumbers.ts
+++ pages/api/getBusNumbers.ts
@@ -5,7 +5,7 @@ import { transferChildProcessOutput } from "../../utils/shell";
 
 // get adb devices
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-    const _path: string = path.join(process.cwd(), 'scripts/get_bus_numbers.sh');
+    const _path: string = path.join(process.cwd(), 'scripts', 'get_bus_numbers.sh');
     const cmd: ChildProcessWithoutNullStreams = spawn(_path, { shell: true });
     transferChildProcessOutput(cmd, res);
 }
--- pages/api/getConfig.ts
+++ pages/api/getConfig.ts
@@ -10,8 +10,8 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     if (!username || username.trim() === "") {
         return res.end("[ERROR] Can not read file when username is not provided");
     }
-    const _path: string = path.join(process.cwd(), 'scripts/get_config.py');
-    const cmd: ChildProcessWithoutNullStreams = spawn(`python3 ${_path}`, { shell: true });
+    const _path: string = path.join(process.cwd(), 'scripts', 'get_config.py');
+    const cmd: ChildProcessWithoutNullStreams = spawn(`python ${_path}`, { shell: true });
     log(`[INFO] Getting config for ${username} ...`);
     cmd.stdin.write(username);
     cmd.stdin.end();
